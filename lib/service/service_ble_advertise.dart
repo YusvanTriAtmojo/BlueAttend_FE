8a7ccf9a86a93eda50d0cead95d337643d368185
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2026-09-05 08:08:18 +0700

    Menambahkan nilai uuid untuk memulai advertise ble

--- lib/service/service_ble_advertise.dart
+++ lib/service/service_ble_advertise.dart
@@ -3,8 +3,13 @@ import 'package:flutter/services.dart';
 class ServiceBleAdvertiser {
   static const MethodChannel _channel = MethodChannel('ble_advertiser');
 
-  static Future<void> startAdvertising() async {
-    await _channel.invokeMethod('startAdvertising');
+  static Future<void> startAdvertising(String uuid) async {
+    await _channel.invokeMethod(
+      'startAdvertising',
+      {
+        'uuid': uuid,
+      },
+    );
   }
 
   static Future<void> stopAdvertising() async {
