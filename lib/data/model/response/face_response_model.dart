ad99657896340f70aaea7fa7976b758f2ab12bc8
Yusvan12 <126647590+Yusvan12@users.noreply.github.com>
2026-09-11 20:17:21 +0700

    Menambahkan data foto_profile dan face embedding pada face response model

--- lib/data/model/response/face_response_model.dart
+++ lib/data/model/response/face_response_model.dart
@@ -3,19 +3,27 @@ import 'dart:convert';
 class FaceResponseModel {
   final int statusCode;
   final String message;
+  final String? fotoProfile;
+  final int? embeddingDimension;
 
   FaceResponseModel({
     required this.statusCode,
     required this.message,
+    this.fotoProfile,
+    this.embeddingDimension,
   });
 
   factory FaceResponseModel.fromRawJson(String str) =>
       FaceResponseModel.fromJson(json.decode(str));
 
   factory FaceResponseModel.fromJson(Map<String, dynamic> json) {
+    final data = json["data"];
+
     return FaceResponseModel(
-      statusCode: json["status_code"],
-      message: json["message"],
+      statusCode: json["status_code"] ?? 0,
+      message: json["message"] ?? "",
+      fotoProfile: data?["foto_profile"],
+      embeddingDimension: data?["embedding"],
     );
   }
 }
